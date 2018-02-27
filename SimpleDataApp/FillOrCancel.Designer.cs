92186bbe5ff9a7f230e99986ce0a4cbb97f23094
zendruid <zendruid@hotmail.com>
2018-02-27 14:34:56 -0500

    Added all controls to the 3 forms

--- SimpleDataApp/FillOrCancel.Designer.cs
+++ SimpleDataApp/FillOrCancel.Designer.cs
@@ -28,11 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbOrderID = new System.Windows.Forms.TextBox();
+            this.lblOrderID = new System.Windows.Forms.Label();
+            this.btnFindByOrderID = new System.Windows.Forms.Button();
+            this.dtpFillDate = new System.Windows.Forms.DateTimePicker();
+            this.lblFillDate = new System.Windows.Forms.Label();
+            this.dgvCustomerOrders = new System.Windows.Forms.DataGridView();
+            this.btnCancelOrder = new System.Windows.Forms.Button();
+            this.btnFillOrder = new System.Windows.Forms.Button();
+            this.btnFinishUpdates = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbOrderID
+            // 
+            this.tbOrderID.Location = new System.Drawing.Point(142, 31);
+            this.tbOrderID.Name = "tbOrderID";
+            this.tbOrderID.Size = new System.Drawing.Size(100, 20);
+            this.tbOrderID.TabIndex = 0;
+            // 
+            // lblOrderID
+            // 
+            this.lblOrderID.AutoSize = true;
+            this.lblOrderID.Location = new System.Drawing.Point(86, 34);
+            this.lblOrderID.Name = "lblOrderID";
+            this.lblOrderID.Size = new System.Drawing.Size(50, 13);
+            this.lblOrderID.TabIndex = 1;
+            this.lblOrderID.Text = "Order ID:";
+            // 
+            // btnFindByOrderID
+            // 
+            this.btnFindByOrderID.Location = new System.Drawing.Point(286, 29);
+            this.btnFindByOrderID.Name = "btnFindByOrderID";
+            this.btnFindByOrderID.Size = new System.Drawing.Size(75, 23);
+            this.btnFindByOrderID.TabIndex = 2;
+            this.btnFindByOrderID.Text = "Find Order";
+            this.btnFindByOrderID.UseVisualStyleBackColor = true;
+            // 
+            // dtpFillDate
+            // 
+            this.dtpFillDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFillDate.Location = new System.Drawing.Point(255, 92);
+            this.dtpFillDate.Name = "dtpFillDate";
+            this.dtpFillDate.Size = new System.Drawing.Size(106, 20);
+            this.dtpFillDate.TabIndex = 3;
+            // 
+            // lblFillDate
+            // 
+            this.lblFillDate.AutoSize = true;
+            this.lblFillDate.Location = new System.Drawing.Point(86, 98);
+            this.lblFillDate.Name = "lblFillDate";
+            this.lblFillDate.Size = new System.Drawing.Size(156, 13);
+            this.lblFillDate.TabIndex = 1;
+            this.lblFillDate.Text = "If filling an order, specify fill date";
+            // 
+            // dgvCustomerOrders
+            // 
+            this.dgvCustomerOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomerOrders.Location = new System.Drawing.Point(12, 131);
+            this.dgvCustomerOrders.Name = "dgvCustomerOrders";
+            this.dgvCustomerOrders.ReadOnly = true;
+            this.dgvCustomerOrders.RowHeadersVisible = false;
+            this.dgvCustomerOrders.Size = new System.Drawing.Size(460, 118);
+            this.dgvCustomerOrders.TabIndex = 4;
+            // 
+            // btnCancelOrder
+            // 
+            this.btnCancelOrder.Location = new System.Drawing.Point(13, 271);
+            this.btnCancelOrder.Name = "btnCancelOrder";
+            this.btnCancelOrder.Size = new System.Drawing.Size(94, 23);
+            this.btnCancelOrder.TabIndex = 5;
+            this.btnCancelOrder.Text = "Cancel Order";
+            this.btnCancelOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnFillOrder
+            // 
+            this.btnFillOrder.Location = new System.Drawing.Point(142, 271);
+            this.btnFillOrder.Name = "btnFillOrder";
+            this.btnFillOrder.Size = new System.Drawing.Size(94, 23);
+            this.btnFillOrder.TabIndex = 5;
+            this.btnFillOrder.Text = "Fill Order";
+            this.btnFillOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnFinishUpdates
+            // 
+            this.btnFinishUpdates.Location = new System.Drawing.Point(378, 271);
+            this.btnFinishUpdates.Name = "btnFinishUpdates";
+            this.btnFinishUpdates.Size = new System.Drawing.Size(94, 23);
+            this.btnFinishUpdates.TabIndex = 5;
+            this.btnFinishUpdates.Text = "Finish";
+            this.btnFinishUpdates.UseVisualStyleBackColor = true;
+            // 
+            // FillOrCancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 321);
+            this.Controls.Add(this.btnFinishUpdates);
+            this.Controls.Add(this.btnFillOrder);
+            this.Controls.Add(this.btnCancelOrder);
+            this.Controls.Add(this.dgvCustomerOrders);
+            this.Controls.Add(this.dtpFillDate);
+            this.Controls.Add(this.lblFillDate);
+            this.Controls.Add(this.btnFindByOrderID);
+            this.Controls.Add(this.lblOrderID);
+            this.Controls.Add(this.tbOrderID);
+            this.Name = "FillOrCancel";
             this.Text = "FillOrCancel";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbOrderID;
+        private System.Windows.Forms.Label lblOrderID;
+        private System.Windows.Forms.Button btnFindByOrderID;
+        private System.Windows.Forms.DateTimePicker dtpFillDate;
+        private System.Windows.Forms.Label lblFillDate;
+        private System.Windows.Forms.DataGridView dgvCustomerOrders;
+        private System.Windows.Forms.Button btnCancelOrder;
+        private System.Windows.Forms.Button btnFillOrder;
+        private System.Windows.Forms.Button btnFinishUpdates;
     }
 }
--- SimpleDataApp/NewCustomer.Designer.cs
+++ SimpleDataApp/NewCustomer.Designer.cs
@@ -29,25 +29,176 @@
         private void InitializeComponent()
         {
             this.gbAddAccount = new System.Windows.Forms.GroupBox();
+            this.tbCustomerName = new System.Windows.Forms.TextBox();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.tbCustomerID = new System.Windows.Forms.TextBox();
+            this.lblCustomerID = new System.Windows.Forms.Label();
+            this.gbCreateOrder = new System.Windows.Forms.GroupBox();
+            this.numOrderAmount = new System.Windows.Forms.NumericUpDown();
+            this.lblOrderAmount = new System.Windows.Forms.Label();
+            this.dtpOrderDate = new System.Windows.Forms.DateTimePicker();
+            this.lblOrderDate = new System.Windows.Forms.Label();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.btnCreateAccount = new System.Windows.Forms.Button();
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnAddAnotherAccount = new System.Windows.Forms.Button();
+            this.gbAddAccount.SuspendLayout();
+            this.gbCreateOrder.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).BeginInit();
             this.SuspendLayout();
             // 
             // gbAddAccount
             // 
+            this.gbAddAccount.Controls.Add(this.lblCustomerID);
+            this.gbAddAccount.Controls.Add(this.lblCustomerName);
+            this.gbAddAccount.Controls.Add(this.tbCustomerID);
+            this.gbAddAccount.Controls.Add(this.tbCustomerName);
             this.gbAddAccount.Location = new System.Drawing.Point(12, 12);
             this.gbAddAccount.Name = "gbAddAccount";
-            this.gbAddAccount.Size = new System.Drawing.Size(217, 80);
+            this.gbAddAccount.Size = new System.Drawing.Size(254, 80);
             this.gbAddAccount.TabIndex = 0;
             this.gbAddAccount.TabStop = false;
             this.gbAddAccount.Text = "Add Account";
+            // 
+            // tbCustomerName
+            // 
+            this.tbCustomerName.Location = new System.Drawing.Point(96, 19);
+            this.tbCustomerName.Name = "tbCustomerName";
+            this.tbCustomerName.Size = new System.Drawing.Size(152, 20);
+            this.tbCustomerName.TabIndex = 0;
+            // 
+            // lblCustomerName
+            // 
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Location = new System.Drawing.Point(8, 22);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(82, 13);
+            this.lblCustomerName.TabIndex = 1;
+            this.lblCustomerName.Text = "Customer Name";
+            // 
+            // tbCustomerID
+            // 
+            this.tbCustomerID.Location = new System.Drawing.Point(96, 45);
+            this.tbCustomerID.Name = "tbCustomerID";
+            this.tbCustomerID.ReadOnly = true;
+            this.tbCustomerID.Size = new System.Drawing.Size(152, 20);
+            this.tbCustomerID.TabIndex = 0;
+            // 
+            // lblCustomerID
+            // 
+            this.lblCustomerID.AutoSize = true;
+            this.lblCustomerID.Location = new System.Drawing.Point(8, 48);
+            this.lblCustomerID.Name = "lblCustomerID";
+            this.lblCustomerID.Size = new System.Drawing.Size(65, 13);
+            this.lblCustomerID.TabIndex = 1;
+            this.lblCustomerID.Text = "Customer ID";
+            // 
+            // gbCreateOrder
+            // 
+            this.gbCreateOrder.Controls.Add(this.dtpOrderDate);
+            this.gbCreateOrder.Controls.Add(this.lblOrderDate);
+            this.gbCreateOrder.Controls.Add(this.lblOrderAmount);
+            this.gbCreateOrder.Controls.Add(this.numOrderAmount);
+            this.gbCreateOrder.Location = new System.Drawing.Point(12, 99);
+            this.gbCreateOrder.Name = "gbCreateOrder";
+            this.gbCreateOrder.Size = new System.Drawing.Size(254, 100);
+            this.gbCreateOrder.TabIndex = 1;
+            this.gbCreateOrder.TabStop = false;
+            this.gbCreateOrder.Text = "Create Order";
+            // 
+            // numOrderAmount
+            // 
+            this.numOrderAmount.Location = new System.Drawing.Point(96, 20);
+            this.numOrderAmount.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numOrderAmount.Name = "numOrderAmount";
+            this.numOrderAmount.Size = new System.Drawing.Size(120, 20);
+            this.numOrderAmount.TabIndex = 0;
+            // 
+            // lblOrderAmount
+            // 
+            this.lblOrderAmount.AutoSize = true;
+            this.lblOrderAmount.Location = new System.Drawing.Point(18, 22);
+            this.lblOrderAmount.Name = "lblOrderAmount";
+            this.lblOrderAmount.Size = new System.Drawing.Size(72, 13);
+            this.lblOrderAmount.TabIndex = 1;
+            this.lblOrderAmount.Text = "Order Amount";
+            // 
+            // dtpOrderDate
+            // 
+            this.dtpOrderDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpOrderDate.Location = new System.Drawing.Point(96, 47);
+            this.dtpOrderDate.Name = "dtpOrderDate";
+            this.dtpOrderDate.Size = new System.Drawing.Size(120, 20);
+            this.dtpOrderDate.TabIndex = 2;
+            // 
+            // lblOrderDate
+            // 
+            this.lblOrderDate.AutoSize = true;
+            this.lblOrderDate.Location = new System.Drawing.Point(31, 53);
+            this.lblOrderDate.Name = "lblOrderDate";
+            this.lblOrderDate.Size = new System.Drawing.Size(59, 13);
+            this.lblOrderDate.TabIndex = 1;
+            this.lblOrderDate.Text = "Order Date";
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(62, 241);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(75, 23);
+            this.btnFinish.TabIndex = 2;
+            this.btnFinish.Text = "Finish";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            // 
+            // btnCreateAccount
+            // 
+            this.btnCreateAccount.Location = new System.Drawing.Point(302, 69);
+            this.btnCreateAccount.Name = "btnCreateAccount";
+            this.btnCreateAccount.Size = new System.Drawing.Size(119, 23);
+            this.btnCreateAccount.TabIndex = 2;
+            this.btnCreateAccount.Text = "Create Account";
+            this.btnCreateAccount.UseVisualStyleBackColor = true;
+            // 
+            // btnPlaceOrder
+            // 
+            this.btnPlaceOrder.Location = new System.Drawing.Point(302, 176);
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.Size = new System.Drawing.Size(119, 23);
+            this.btnPlaceOrder.TabIndex = 2;
+            this.btnPlaceOrder.Text = "Place Order";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnAddAnotherAccount
+            // 
+            this.btnAddAnotherAccount.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAddAnotherAccount.Location = new System.Drawing.Point(239, 251);
+            this.btnAddAnotherAccount.Name = "btnAddAnotherAccount";
+            this.btnAddAnotherAccount.Size = new System.Drawing.Size(182, 23);
+            this.btnAddAnotherAccount.TabIndex = 2;
+            this.btnAddAnotherAccount.Text = "Add Another Account";
+            this.btnAddAnotherAccount.UseVisualStyleBackColor = true;
             // 
             // NewCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(508, 306);
+            this.Controls.Add(this.btnAddAnotherAccount);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Controls.Add(this.btnCreateAccount);
+            this.Controls.Add(this.btnFinish);
+            this.Controls.Add(this.gbCreateOrder);
             this.Controls.Add(this.gbAddAccount);
             this.Name = "NewCustomer";
             this.Text = "NewCustomer";
+            this.gbAddAccount.ResumeLayout(false);
+            this.gbAddAccount.PerformLayout();
+            this.gbCreateOrder.ResumeLayout(false);
+            this.gbCreateOrder.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOrderAmount)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -55,5 +206,18 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gbAddAccount;
+        private System.Windows.Forms.Label lblCustomerID;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.TextBox tbCustomerID;
+        private System.Windows.Forms.TextBox tbCustomerName;
+        private System.Windows.Forms.GroupBox gbCreateOrder;
+        private System.Windows.Forms.DateTimePicker dtpOrderDate;
+        private System.Windows.Forms.Label lblOrderDate;
+        private System.Windows.Forms.Label lblOrderAmount;
+        private System.Windows.Forms.NumericUpDown numOrderAmount;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Button btnCreateAccount;
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnAddAnotherAccount;
     }
 }
